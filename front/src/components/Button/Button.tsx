e02fe42310573b3e619bb81a4eea9eb9fef75bf5
Gabriel de Almeida Machado <gdeamachado@gmail.com>
2024-07-13 09:34:23 -0300

    Criação do Cabeçalho

--- front/src/components/Button/Button.tsx
+++ front/src/components/Button/Button.tsx
@@ -4,11 +4,12 @@ import styles from './Button.module.css';
 type ButtonProps = {
   title: string;
   showArrow?: boolean;
+  fullWidth?: boolean; // Adicione a prop fullWidth
 };
 
-const Button: React.FC<ButtonProps> = ({ title, showArrow = false }) => {
+const Button: React.FC<ButtonProps> = ({ title, showArrow = false, fullWidth = false }) => {
   return (
-    <button className={styles.button}>
+    <button className={`${styles.button} ${fullWidth ? styles.buttonFullWidth : ''}`}>
       {title}
       {showArrow && <img src="/icons/arrow.svg" alt="arrow" className={styles.arrow} />}
     </button>
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header/Header';
 import IconStack from '../components/IconStack/IconStack';
+import Button from '../components/Button/Button';
 
 const Home = () => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const handleEmailChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError('Por favor, insira um e-mail válido.');
+    } else {
+      setError('');
+      // Lógica para enviar o e-mail
+      console.log('Email enviado:', email);
+    }
+  };
+
+  const validateEmail = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +44,17 @@ const Home = () => {
             <IconStack />
             <h1 className={styles.heroTitle}>O seu negócio inteiro em uma plataforma.</h1>
             <h2 className={styles.heroSubtitle}>Simples, prático e tecnológico</h2>
+            <form className={styles.emailForm} onSubmit={handleSubmit}>
+              <input
+                type="email"
+                placeholder="Seu e-mail de trabalho"
+                className={styles.emailInput}
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <Button title="Começar Agora" fullWidth />
+            </form>
+            {error && <p className={styles.error}>{error}</p>}
           </div>
         </section>
         <h1>Home Page</h1>
